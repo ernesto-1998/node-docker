659cebed162fcb01ec453ed33d0f46af70dd65a2
ernesto-1998 <ernestoalonso1998@gmail.com>
2022-07-13 16:27:36 -0600

    2:33:19

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
 const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+    mongoose.connect(mongoURL)
+    .then(() => console.log("Succesfully connected to DB"))
+    .catch((e) => {
+        console.log(e);
+        setTimeout(connectWithRetry, 5000);
+    })
+}
+
+connectWithRetry();
 
 app.get("/", (req, res) => res.send(`<h1>Hi Docker Fellows!!!</h1>`))
 
