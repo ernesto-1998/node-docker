7aa212f57f148f0157edb8c8d1c946905bf47699
ernesto-1998 <ernestoalonso1998@gmail.com>
2022-07-18 00:46:34 -0600

    3:58:26

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const redis = require('redis');
+const cors = require("cors");
 let RedisStore = require('connect-redis')(session);
 
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, REDIS_URL, REDIS_PORT, SESSION_SECRET } = require('./config/config');
@@ -34,6 +35,8 @@ let redisClient = redis.createClient({
 
 redisClient.connect().catch(console.error)
 
+app.enable("trust proxy");
+app.use(cors({}));
 app.use(session({
     store: new RedisStore({ client: redisClient }),
     secret: SESSION_SECRET,
@@ -49,7 +52,10 @@ app.use(session({
 app.use(express.json());
 // app.use(express.urlencoded({extended: false}));
 
-app.get("/", (req, res) => res.send(`<h1>Hi Docker Fellows!!!</h1>`));
+app.get("/api/v1", (req, res) => {
+    console.log("Yeah it run");
+    res.send(`<h1>Hi Docker Fellows!!!</h1>`)
+});
 
 app.use('/api/v1/posts', postRouter);
 app.use('/api/v1/users', userRouter);
