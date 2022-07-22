b8b06abe743dc27e7f525910265b59eed5e0036c
ernesto-1998 <ernestoalonso1998@gmail.com>
2022-07-21 20:42:57 -0600

    solving a bug with route signup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.post("/singup", authController.singUp);
+router.post("/signup", authController.singUp);
 router.post("/login", authController.login);
 
 module.exports = router;
